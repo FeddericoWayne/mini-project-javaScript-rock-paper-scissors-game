0a0f1f783ce47ffede486e5077f5811b417e22d7
FeddericoWayne <jackiew1120@hotmail.com>
2022-11-17 23:51:00 -0500

    tweaked code

--- script.js
+++ script.js
@@ -4,27 +4,40 @@ var loses = 0;
 window.alert('Wanna Play Rock, Paper, Scissors?');
 window.alert('Enter "r" for Rock, "p" for Paper, and "s" for scissors!');
 
-
-
-
 var playGame = function() {
 
-    var computerOptions = ['r', 'p', 's'];
+    var computerOptions = ['Rock', 'Paper', 'Scissors'];
     
     var computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
     var playerChoice = window.prompt("Enter Your Choice");
 
-    if (computerChoice === "r" && playerChoice === "p") {
+    if (!playerChoice) {
+        alert("See You Next Time!");
+        return;
+    }
+
+    var playerChoiceUpper = playerChoice.toUpperCase();
+
+    if (playerChoice.toUpperCase() === "R") {
+        var playerChoiceFull = "Rock";
+    } else if (playerChoice.toUpperCase() === "P") {
+        var playerChoiceFull = "Paper";
+    } else {
+        var playerChoiceFull = "Scissors";
+    }
+    
+
+    if (computerChoice === "Rock" && playerChoiceFull === "Paper") {
         alert('The Computer Chooses Rock. You Win!');
         wins++;
-    } else if (computerChoice === "p" && playerChoice === "s") {
+    } else if (computerChoice === "Paper" && playerChoiceFull === "Scissors") {
         alert('The Computer Chooses Paper. You Win!');
         wins++;
-    } else if (computerChoice === "s" && playerChoice === "r") {
+    } else if (computerChoice === "Scissors" && playerChoiceFull === "Rock") {
         alert('The Computer Chooses Scissors. You Win!');
         wins++;
-    } else if (computerChoice === playerChoice) {
-        alert("The Computer Also Chooses " + playerChoice + ". It's a Tie!");
+    } else if (computerChoice === playerChoiceFull) {
+        alert("The Computer Also Chooses " + playerChoiceFull + ". It's a Tie!");
     } else {
         alert ('The Computer Chooses ' + computerChoice + ". You Lose!");
         loses++;
@@ -32,7 +45,8 @@ var playGame = function() {
 
     alert("Your Win Count: " + wins + " ; Your Lose Count: " + loses);
 
-    if (window.confirm ("Play Again?")) {
+    var playAgain = window.confirm("Play Again?");
+    if (playAgain) {
         playGame();
     } else {
         alert("Thanks for Playing!");
